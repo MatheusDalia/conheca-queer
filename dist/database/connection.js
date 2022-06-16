a3046d2ac56e591efb70b5b901c92a3851bc22a9
matheusdalia <matheusdalia8@gmail.com>
2022-06-15 22:43:50 -0300

    client deploy

--- dist/database/connection.js
+++ dist/database/connection.js
@@ -17,7 +17,7 @@ const connection = new _typeorm.DataSource({
   name: 'default',
   type: "postgres",
   host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),
+  port: Number(process.env.DATABASE_PORT) || 3001,
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_DB,
